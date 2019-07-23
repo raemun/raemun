7fd6a64c5aa214267e1ac20238255b76c0e0d1b8
raemun <52664823+raemun@users.noreply.github.com>
2019-07-23 14:50:54 +0300

    Add files via upload

---
+++ Untitled4.cpp
@@ -0,0 +1,18 @@
+#include <locale.h>
+
+main()
+{
+	int i,n,SC=0;
+	
+	printf("her bir dizinin eleman sayisi : ");
+	scanf("%d", &n);
+	int A[n],B[n];
+	
+	for (i=0;i<n;++i)
+	{
+		printf("A ve B dizilerinin %d. elemanlari : ",i+1);
+		scanf("%d %d",&A[i],&B[i]);
+		SC+=A[i]*B[i];
+	}
+	printf("Skaler carpim : %d",SC);
+}
